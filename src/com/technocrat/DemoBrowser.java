eb0c181f0a74fe1dd60dd08051cb8b35a3b7a8df
Rathod <rathodanil.s@gmail.com>
2023-02-24 20:14:14 +0530

    initial commit

---
+++ src/com/technocrat/DemoBrowser.java
@@ -0,0 +1,34 @@
+package com.technocrat;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DemoBrowser {
+
+	public static void main(String[] args) throws Exception {
+		
+		DemoBrowser browser = new DemoBrowser();
+		browser.launchBrowser();
+		
+	}
+	
+	public void launchBrowser() throws Exception {
+		/*
+		 * ChromeDriver driver = new ChromeDriver();
+		 * 
+		 * driver.get("https://www.selenium.dev"); driver.manage().window().maximize();
+		 * Thread.sleep(5000);
+		 * 
+		 * String title = driver.getTitle(); System.out.println(title);
+		 * 
+		 * 
+		 * driver.close(); Thread.sleep(5000);
+		 * 
+		 * driver.quit(); Thread.sleep(5000);
+		 */
+		
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("https://www.selenium.dev");
+		
+	}
+}
