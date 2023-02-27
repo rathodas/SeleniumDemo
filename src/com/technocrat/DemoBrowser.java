1cf1f18afc27b289cb7c49d5b123e163e50dade4
Anil <rathodanil.s@gmail.com>
2023-02-27 20:56:31 +0530

    changes

--- src/com/technocrat/DemoBrowser.java
+++ src/com/technocrat/DemoBrowser.java
@@ -13,29 +13,7 @@ public class DemoBrowser {
 	}
 	
 	public void launchBrowser() throws Exception {
-		/*
-		 * ChromeDriver driver = new ChromeDriver();
-		 * 
-		 * driver.get("https://www.selenium.dev"); driver.manage().window().maximize();
-		 * Thread.sleep(5000);
-		 * 
-		 * String title = driver.getTitle(); System.out.println(title);
-		 * 
-		 * 
-		 * driver.close(); Thread.sleep(5000);
-		 * 
-		 * driver.quit(); Thread.sleep(5000);
-		 */
 		
-		/*
-		 * FirefoxDriver driver = new FirefoxDriver();
-		 * driver.get("https://www.selenium.dev"); driver.quit();
-		 */
-		
-		InternetExplorerDriver driver = new InternetExplorerDriver();
-		
-		driver.get("https://www.selenium.dev");
-		driver.quit();
 		
 	}
 }
